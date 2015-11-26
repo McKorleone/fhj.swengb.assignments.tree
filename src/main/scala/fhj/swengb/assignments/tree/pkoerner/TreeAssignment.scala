5e43f2e0011c098fc194e82284ce0e46b29d2603
McKorleone <paul.koerner@edu.fh-joanneum.at>
2015-11-26 17:36:35 +0100

    Commit Homework

--- src/main/scala/fhj/swengb/assignments/tree/pkoerner/TreeAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/tree/pkoerner/TreeAssignment.scala
@@ -2,72 +2,12 @@ package fhj.swengb.assignments.tree.pkoerner
 
 import javafx.scene.paint.Color
 
+import fhj.swengb.assignments.tree.pkoerner.Pt2D
+
+import scala.annotation.tailrec
 import scala.math.BigDecimal.RoundingMode
 import scala.util.Random
 
-object Graph {
-
-  val colorMap =
-    Map[Int, Color](
-      0 -> Color.ROSYBROWN,
-      1 -> Color.BROWN,
-      2 -> Color.SADDLEBROWN,
-      3 -> Color.INDIANRED,
-      4 -> Color.DARKGREEN,
-      5 -> Color.GREEN,
-      6 -> Color.YELLOWGREEN,
-      7 -> Color.GREENYELLOW,
-      8 -> Color.YELLOW
-    )
-
-  /**
-    * creates a random tree
-    *
-    * @param pt
-    * @return
-    */
-  def randomTree(pt: Pt2D): Tree[L2D] =
-    mkGraph(pt, Random.nextInt(360), Random.nextDouble() * 150, Random.nextInt(7))
-
-
-  /**
-    * Given a Tree of L2D's and a function which can convert any L2D to a Line,
-    * you have to traverse the tree (visit all nodes) and create a sequence
-    * of Line's. The ordering of the lines is not important.
-    *
-    * @param tree  a tree which contains L2D instances
-    * @param convert a converter function
-    * @return
-    */
-  def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] = {
-  ???
-  }
-
-  /**
-    * Creates a tree graph.
-    *
-    * @param start the startpoint (root) of the tree
-    * @param initialAngle initial angle of the tree
-    * @param length the initial length of the tree
-    * @param treeDepth the depth of the tree
-    * @param factor the factor which the length is decreasing for every iteration
-    * @param angle the angle between a branch and the root
-    * @param colorMap color map, by default it is the colormap given in the companion object Graph
-    *
-    * @return a Tree[L2D] which can be traversed by other algorithms
-    */
-  def mkGraph(start: Pt2D,
-              initialAngle: AngleInDegrees,
-              length: Double,
-              treeDepth: Int,
-              factor: Double = 0.75,
-              angle: Double = 45.0,
-              colorMap: Map[Int, Color] = Graph.colorMap): Tree[L2D] = {
-    assert(treeDepth <= colorMap.size, s"Treedepth higher than color mappings - bailing out ...")
-    ???
- }
-
-}
 
 object MathUtil { //start with
 
@@ -78,7 +18,12 @@ object MathUtil { //start with
     * @return
     */
   def round(value: Double): Double = {
-    ???
+    scala.math.round(value * 1000) / 1000
+  }
+
+  def translate(point: Pt2D, angle: AngleInDegrees, length: Double) = {
+    val angleInRadiants:AngleInRadiants = MathUtil.toRadiants(angle)
+    Pt2D(point.x + scala.math.cos(angleInRadiants), point.y + scala.math.sin(angleInRadiants))
   }
 
   /**
@@ -88,7 +33,7 @@ object MathUtil { //start with
     * @return
     */
   def toRadiants(angle: AngleInDegrees): AngleInRadiants = {
-   ???
+    (angle / 180) * scala.math.Pi
   }
 }
 
@@ -110,7 +55,7 @@ object L2D {
     * how can we convert start point and angle into to points
     */
   def apply(start: Pt2D, angle: AngleInDegrees, length: Double, color: Color): L2D = {
-    ???
+    L2D(start, MathUtil.translate(start, angle, length), color)
   }
 
 
@@ -147,6 +92,103 @@ case class L2D(start: Pt2D, end: Pt2D, color: Color) {
     L2D(end, angle + deltaAngle, length * factor, c)
   }
 
-
 }
 
+object Graph {
+
+  val colorMap =
+    Map[Int, Color](
+      0 -> Color.ROSYBROWN,
+      1 -> Color.BROWN,
+      2 -> Color.SADDLEBROWN,
+      3 -> Color.INDIANRED,
+      4 -> Color.DARKGREEN,
+      5 -> Color.GREEN,
+      6 -> Color.YELLOWGREEN,
+      7 -> Color.GREENYELLOW,
+      8 -> Color.YELLOW
+    )
+
+  /**
+    * creates a random tree
+    *
+    * @param pt
+    * @return
+    */
+  def randomTree(pt: Pt2D): Tree[L2D] =
+    mkGraph(pt, Random.nextInt(360), Random.nextDouble() * 150, Random.nextInt(7))
+
+
+  /**
+    * Given a Tree of L2D's and a function which can convert any L2D to a Line,
+    * you have to traverse the tree (visit all nodes) and create a sequence
+    * of Line's. The ordering of the lines is not important.
+    *
+    * @param tree  a tree which contains L2D instances
+    * @param convert a converter function
+    * @return
+    */
+  def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] = tree match {
+    case Node(value) => Seq(convert(value))
+    case Branch(left, right) => traverse(left)(convert) ++ traverse(right)(convert)
+
+      /**
+        * Creates a tree graph.
+        *
+        * @param start the startpoint (root) of the tree
+        * @param initialAngle initial angle of the tree
+        * @param length the initial length of the tree
+        * @param treeDepth the depth of the tree
+        * @param factor the factor which the length is decreasing for every iteration
+        * @param angle the angle between a branch and the root
+        * @param colorMap color map, by default it is the colormap given in the companion object Graph
+        *
+        * @return a Tree[L2D] which can be traversed by other algorithms
+        */
+      def mkGraph(start: Pt2D,
+                  initialAngle: AngleInDegrees,
+                  length: Double,
+                  treeDepth: Int,
+                  factor: Double = 0.75,
+                  angle: Double = 45.0,
+                  colorMap: Map[Int, Color] = Graph.colorMap): Tree[L2D] = {
+        assert(treeDepth <= colorMap.size, s"Treedepth higher than color mappings - bailing out ...")
+
+        def treeBuilder(currentPoint: Pt2D, currentDepth: Int, currentLength: Double, currentAngle: AngleInDegrees): Tree[L2D] = {
+          currentDepth match {
+            case treeDepth =>
+              Branch(
+                Node(L2D(currentPoint, initialAngle - angle, currentLength, colorMap(currentDepth))),
+                Node(L2D(currentPoint, initialAngle + angle, currentLength, colorMap(currentDepth)))
+              )
+            case _ =>
+              Branch(
+                Node(L2D(currentPoint, initialAngle, currentLength, colorMap(currentDepth))),
+                Branch(
+                  Branch(
+                    Node(L2D(currentPoint, initialAngle - angle, currentLength, colorMap(currentDepth))),
+                    treeBuilder(
+                      MathUtil.translate(currentPoint, initialAngle - angle, currentLength),
+                      currentDepth + 1,
+                      currentLength * factor,
+                      initialAngle - angle
+                    )
+                  ),
+                  Branch(
+                    Node(L2D(currentPoint, initialAngle + angle, currentLength, colorMap(currentDepth))),
+                    treeBuilder(
+                      MathUtil.translate(currentPoint, initialAngle + angle, currentLength),
+                      currentDepth + 1,
+                      currentLength * factor,
+                      initialAngle + angle
+                    )
+                  )
+                )
+              )
+          }
+        }
+        treeBuilder(start, 0, length, initialAngle)
+      }
+
+  }
+}
