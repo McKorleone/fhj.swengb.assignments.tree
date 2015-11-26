141e16368a2ea386470c1a536109d52a0ef63653
McKorleone <paul.koerner@edu.fh-joanneum.at>
2015-11-26 17:41:01 +0100

    Correction: Curlybracket

--- src/main/scala/fhj/swengb/assignments/tree/pkoerner/TreeAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/tree/pkoerner/TreeAssignment.scala
@@ -131,7 +131,7 @@ object Graph {
   def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] = tree match {
     case Node(value) => Seq(convert(value))
     case Branch(left, right) => traverse(left)(convert) ++ traverse(right)(convert)
-
+  }
       /**
         * Creates a tree graph.
         *
@@ -191,4 +191,3 @@ object Graph {
       }
 
   }
-}
